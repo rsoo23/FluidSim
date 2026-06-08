37d063956d200d11141c77c93718abdfb59498dc
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-06-08 22:40:38 +0800

    feat: add mouse button and cursor position callbacks to register mouse dragging

--- FluidSim/src/core/Application.cpp
+++ FluidSim/src/core/Application.cpp
@@ -7,6 +7,34 @@ namespace {
 	{
 		glViewport(0, 0, width, height);
 	};
+
+	bool isDragging = false;
+	double xPos = 0;
+	double yPos = 0;
+
+	void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+	{
+		if (button == GLFW_MOUSE_BUTTON_LEFT)
+		{
+			if (action == GLFW_PRESS)
+			{
+				isDragging = true;
+			}
+			else if (action == GLFW_RELEASE)
+			{
+				isDragging = false;
+			}
+		}
+	}
+
+	void cursor_position_callback(GLFWwindow* window, double xPos, double yPos)
+	{
+		if (isDragging)
+		{
+			glfwGetCursorPos(window, &xPos, &yPos);
+			std::cout << "x: " << xPos << ", y: " << yPos << "\n";
+		}
+	}
 }
 
 Application::Application(int versionMajor, int versionMinor, int width, int height, std::string_view title)
@@ -55,6 +83,9 @@ void Application::processInput()
 {
 	if (glfwGetKey(m_Window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(m_Window, true);
+
+	glfwSetCursorPosCallback(m_Window, cursor_position_callback);
+	glfwSetMouseButtonCallback(m_Window, mouse_button_callback);
 }
 
 Application::~Application()
