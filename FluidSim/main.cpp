9fd4b142920e218975b847ba8bad3cc593fe459a
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-06-08 16:21:48 +0800

    feat: add texture wrapping and filters

--- FluidSim/main.cpp
+++ FluidSim/main.cpp
@@ -76,6 +76,12 @@ int main()
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 	// VAO, VBO
 	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
