9b43328022e58e497843aaca7ee6187fc7a0a247
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-06-11 00:54:49 +0800

    chore(Shader): remove unused methods, move programId to private

--- FluidSim/src/shader/Shader.cpp
+++ FluidSim/src/shader/Shader.cpp
@@ -66,21 +66,6 @@ void Shader::use()
 	glUseProgram(programId);
 }
 
-void Shader::setBool(const std::string& name, bool value) const
-{
-	glUniform1i(glGetUniformLocation(programId, name.c_str()), static_cast<int>(value));
-}
-
-void Shader::setInt(const std::string& name, int value) const
-{
-	glUniform1i(glGetUniformLocation(programId, name.c_str()), value);
-}
-
-void Shader::setFloat(const std::string& name, float value) const
-{
-	glUniform1f(glGetUniformLocation(programId, name.c_str()), value);
-}
-
 void Shader::checkShaderError(unsigned int shaderId, ShaderType type) const
 {
 	int success;
--- FluidSim/src/shader/Shader.hpp
+++ FluidSim/src/shader/Shader.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <glad/glad.h> // include glad to get all the required OpenGL headers
-  
+#include <glad/glad.h>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -11,21 +10,18 @@
 class Shader
 {
 public:
-    // the program ID
-    unsigned int programId;
-  
-    // constructor reads and builds the shader
     Shader(const std::filesystem::path& vertexPath, const std::filesystem::path& fragmentPath);
 
-    // use / activate the shader
+	Shader()                 			= delete;
+	Shader(const Shader&)				= delete;
+	Shader& operator=(const Shader&)	= delete;
+	Shader(Shader&&)					= delete;
+	Shader& operator=(Shader&&)		    = delete;
+
     void use();
 
-    // utility uniform functions
-    void setBool(const std::string &name, bool value) const;  
-    void setInt(const std::string &name, int value) const;   
-    void setFloat(const std::string &name, float value) const;
-
 private:
+    unsigned int programId;
     enum class ShaderType {
         VERTEX,
         FRAGMENT,
