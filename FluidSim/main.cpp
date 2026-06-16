fe093439d26145edbd67f4f1d73a064d57ca750f
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-06-16 23:04:47 +0800

    feat: add input validation with static assert

--- FluidSim/main.cpp
+++ FluidSim/main.cpp
@@ -7,20 +7,38 @@ int main()
 	constexpr unsigned int versionMajor{ 4 };
 	constexpr unsigned int versionMinor{ 6 };
 	constexpr unsigned int width{ 800 };
-	constexpr unsigned int height{ 600 };
+	constexpr unsigned int height{ 800 };
 
 	const std::filesystem::path vertexRelPath{ R"(shaders\shader.vert)" };
 	const std::filesystem::path fragRelPath{ R"(shaders\fire.frag)" };
 
 	constexpr unsigned int jacobiIterations{ 40u };
-	constexpr float diffusionCoeff{ 2.f };
-	constexpr float viscosityCoeff{ 0.25f };
-	constexpr float densityIncrement{ 0.2f };
-	constexpr float cursorRadius{ 10.f };
-	constexpr float forceMultiplier{ 1000.f };
+	constexpr float diffusionCoeff{ 1.f };
+	constexpr float viscosityCoeff{ 0.1f };
+	constexpr float densityIncrement{ 0.1f };
+	constexpr float cursorRadius{ 20.f };
+	constexpr float forceMultiplier{ 100.f };
 	constexpr float densityIncrementMultiplier{ 100.f };
 
-	constexpr float vorticityCoeff{ 40.f };
+	constexpr float vorticityCoeff{ 1.f };
+
+	static_assert(jacobiIterations > 0,           			"jacobiIterations must be > 0");
+	static_assert(jacobiIterations <= 40,          			"jacobiIterations must be <= 40");
+	static_assert(diffusionCoeff > 0.f,           			"diffusionCoeff must be > 0.0");
+	static_assert(diffusionCoeff <= 1.f,          			"diffusionCoeff must be <= 1.0");
+	static_assert(viscosityCoeff > 0.f,           			"viscosityCoeff must be > 0.0");
+	static_assert(viscosityCoeff <= 1.f,          			"viscosityCoeff must be <= 1.0");
+	static_assert(densityIncrement > 0.f,         			"densityIncrement must be > 0.0");
+	static_assert(densityIncrement <= 1.f,					"densityIncrement must be <= 1.0");
+	static_assert(cursorRadius > 0.f,             	    	"cursorRadius must be > 0.0");
+	static_assert(cursorRadius < float(width) / 10.f,    	"cursorRadius must be < width / 10");
+	static_assert(cursorRadius < float(height) / 10.f,   	"cursorRadius must be < height / 10");
+	static_assert(forceMultiplier > 0.f,          	    	"forceMultiplier must be > 0.0");
+	static_assert(forceMultiplier <= 10000.f,           	"force_multiplier must be <= 10000.0");
+	static_assert(densityIncrementMultiplier > 0.f,     	"densityIncrementMultiplier must be > 0.0");
+	static_assert(densityIncrementMultiplier <= 10000.f,	"densityIncrementMultiplier must be <= 10000.0");
+	static_assert(vorticityCoeff >= 0.f,          			"vorticityCoeff must be > 0.0");
+	static_assert(vorticityCoeff <= 1.f,         			"vorticityCoeff must be <= 1.0");
 
 	try
 	{
