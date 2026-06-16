54937527fba6b49e696ea0fb71ab33d6c3596535
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-06-16 22:33:53 +0800

    fix(FluidSim): upgrade glTexParameteri to glTextureParameteri

--- FluidSim/src/simulation/FluidSim.cpp
+++ FluidSim/src/simulation/FluidSim.cpp
@@ -202,11 +202,10 @@ GLuint FluidSim::generateTexture() const
 	glCreateTextures(GL_TEXTURE_2D, 1, &texId);
 	glTextureStorage2D(texId, 1, GL_R32F, static_cast<GLsizei>(m_ScreenWidth), static_cast<GLsizei>(m_ScreenHeight));
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTextureParameteri(texId, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(texId, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(texId, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(texId, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	return texId;
 }
